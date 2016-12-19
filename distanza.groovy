924ec770134a3414bdb26ee0f0153c3fd7fc9a8e
Omar Rampado <omar.rampado@larus-ba.it>
2016-12-19 12:15:18 +0100

    Generated interpolation points

--- distanza.groovy
+++ distanza.groovy
@@ -1,10 +1,10 @@
 
 
 
-def lat1 = 43.7771147
-def lon1 = 11.2561717
-def lat2 = 43.7784822
-def lon2 = 11.2572432
+def lat1 = Double.valueOf("43.7771147")
+def lon1 = Double.valueOf("11.2561717")
+def lat2 = Double.valueOf("43.7784822")
+def lon2 = Double.valueOf("11.2572432")
 
 
 earthRadius = 6371000 // raggio della terra
@@ -15,3 +15,25 @@ c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
 dist = (earthRadius * c)
 
 println dist
+
+
+def len = Math.sqrt((lon1-lon2)**2 + (lat1-lat2)**2)
+def carLen = 4
+
+int finePointsNumber = dist / carLen	
+println "Punti: $finePointsNumber"
+Double fi = Math.atan2(lon1 - lon2, lat1 - lat2 )
+
+def lonD = lon1-lon2
+def latD = lat1-lat2
+println "b = $lonD = ${len*Math.sin(fi)}"
+def seg = len / finePointsNumber
+finePointsNumber.times
+{
+	
+	Double lat = lat1 + it * seg * Math.cos(fi);
+	Double lon = lon1 + it * seg * Math.sin(fi);
+	
+	println "$lat, $lon ${Math.cos(fi)}"
+}
+
--- osmToPaths.groovy
+++ osmToPaths.groovy
@@ -5,9 +5,10 @@ opendata = new XmlSlurper().parse(inputFile)
 
 nodeMap = [:]
 
+
 class Node{
-	String lat
-	String lon
+	Double lat
+	Double lon
 }
 
 class Street{
@@ -18,9 +19,8 @@ class Street{
 opendata.node.each
 {
 	def n = new Node()
-	n.lat = it.@lat
-	n.lon = it.@lon
-
+	n.lat = Double.valueOf( it.@lat.toString() )
+	n.lon = Double.valueOf( it.@lon.toString() )
 	k = "${it.@id}"
 	nodeMap[k] = n;
 }
@@ -99,5 +99,72 @@ opendata.relation.each
 	}
 }
 
-def json = JsonOutput.toJson(pathList)
+// add fine-grained points
+
+def calculateDistance = { lat1,lon1,lat2,lon2 ->
+	earthRadius = 6371000 // raggio della terra
+	dLat = Math.toRadians(lat2 - lat1)
+	dLng = Math.toRadians(lon2 - lon1)
+	a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.sin(dLng / 2) * Math.sin(dLng / 2)
+	c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+	dist = (earthRadius * c)
+	return dist
+}
+
+def carLen = 4 //meter
+def routes = []
+
+pathList.each
+{
+	def route = new RoutePath()
+	routes.add(route)
+	route.path_id = it.path_id
+
+	it.streets.each
+	{
+		def street = new Street()
+		street.name = it.name
+		route.streets.add(street)
+
+		def finePoints = []
+		def prevPoint = it.points[0]
+		finePoints.add(prevPoint)	
+		it.points.each
+		{
+			Double lat1 = prevPoint.lat
+			Double lon1 = prevPoint.lon
+			Double lat2 = it.lat
+			Double lon2 = it.lon
+			
+			def lenMeter = calculateDistance(lat1,lon1,lat2,lon2)
+			def lenDegree = Math.sqrt((lat1-lat2)**2+(lon1-lon2)**2)
+			int carNum = lenMeter / carLen	
+			
+			if(carNum > 0)
+			{
+				Double carDegree = lenDegree/carNum
+				Double fi = Math.atan2(lon1 - lon2, lat1 - lat2 )
+				Double x = carDegree * Math.cos(fi)
+				Double y = carDegree * Math.sin(fi)
+				carNum.times
+				{
+					Double lat = lat2 + x*it
+					Double lon = lon2 + y*it 
+					
+					def point = new Node()
+					point.lat = lat	
+					point.lon = lon
+					finePoints.add(point)
+				}
+
+			}
+			prevPoint = it
+		}//each point		
+		finePoints.add(prevPoint)
+		street.points = finePoints
+	}//streets
+}
+
+def json = JsonOutput.toJson(routes)
 println JsonOutput.prettyPrint(json)
+
